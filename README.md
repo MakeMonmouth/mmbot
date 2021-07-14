3a7bb6905f95e7c4115ea488f27b4e5dff97c24e
Matthew Macdonald-Wallace <matt@doics.co>
2021-07-14 06:32:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# MMBot
+
+MMBot (no relation to the similar sounding song by Hanson) is the bot who helps with the management of the [Make Monmouth](https://www.makemonmouth.co.uk/) Discord Channel.
+
+## Getting started
+
+1. Clone this repo
+2. Create a virtualenv
+3. Install the requirements (`pip install -r requirements.txt`)
+4. Copy `.env.example` to `.env` and update the values as appropriate
+5. Run the bot (`./mmbot.py`)
+
---
+++ mmbot.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# mmbot.py
+import logging
+import os
+
+logging.basicConfig(level=logging.INFO)
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command(name='create-channel')
+@commands.has_role('@admin')
+async def create_channel(ctx, channel_name='real-python'):
+    guild = ctx.guild
+    existing_channel = discord.utils.get(guild.channels, name=channel_name)
+    if not existing_channel:
+        logging.info(f'Creating a new channel: {channel_name}')
+        await guild.create_text_channel(channel_name)
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send('You do not have the correct role for this command.')
+
+@bot.command(name='meetup')
+async def get_meetups(ctx):
+    logging.info("Meeting Data requested")
+
+bot.run(TOKEN)
