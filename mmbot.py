b8adc04e2f768e455abefb451be6e50950693706
Matthew Macdonald-Wallace <matt@doics.co>
2022-02-18 08:43:28 +0000

    Update the mmbot meetup command to auto-calcuate the next meeting

--- mmbot.py
+++ mmbot.py
@@ -7,10 +7,15 @@ import os
 import aiohttp
 import arrow
 
+import humanize
+
 logging.basicConfig(level=logging.INFO)
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+
+import datetime as dt
+from calendar import monthrange
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -18,13 +23,20 @@ MVENTORY_URI = os.getenv('MVENTORY_URI')
 MVENTORY_TLS_CHECK = os.getenv('MVENTORY_TLS_CHECK') or True
 
 bot = commands.Bot(command_prefix='!')
-api_creds = {
-        "user": os.getenv('TENDENCI_API_USER'),
-        "key": os.getenv('TENDENCI_API_KEY')
-        }
-meeting_uri = f"{os.getenv('TENDENCI_URI')}/api_tasty/v1/event/?format=json"
-tendenci_headers = {"Authorization": f" ApiKey {api_creds['user']}:{api_creds['key']}"}
 
+def first_and_last_thursday(year, month):
+    dates = [dt.date(year, month, x + 1) for x in range(monthrange(year, month)[1])]
+    return [x for x in dates if x.weekday() == 3][0:3:2]
+
+def get_next_meeting():
+    date = dt.date.today()
+    first, second = first_and_last_thursday(date.year, date.month)
+    if date < first:
+        return first
+    if date < second:
+        return second
+    first, _ = first_and_last_thursday(date.year + (1 if date.month == 12 else 0), (date.month % 12) + 1)
+    return first
 
 @bot.command(name='create-channel')
 @commands.has_role('@admin')
@@ -43,41 +55,10 @@ async def on_command_error(ctx, error):
 @bot.command(name='meetup')
 async def meetup(ctx):
     logging.info("Meetup routine called")
-
-    async with aiohttp.ClientSession(headers=tendenci_headers) as session:
-        logging.info("Session Created")
-        async with session.get(meeting_uri) as response:
-            if response.status == 200:
-                logging.info("Request to Tendenci was successful")
-                logging.info("Processing the JSON document")
-                meetings = await response.json()
-                logging.info("JSON Document has been processed")
-                # 2021-09-17T19:30:00+00:00
-                meeting_list = meetings['objects']
-                meeting_list.sort(key = operator.itemgetter('start_dt'))
-                for meeting in meeting_list:
-                    logging.info("Processing meeting")
-                    logging.info(meeting)
-                    if meeting['title'] == "Weekly Meetup!":
-                        logging.info("Meetup found")
-                        start_date =arrow.get( meeting['start_dt'])
-                        end_date =arrow.get( meeting['end_dt'])
-                        venue_name = "Woolly Monmouth"
-                        event_url = f"{os.getenv('TENDENCI_URI')}/events/{meeting['id']}/"
-                        logging.info("Creating message")
-                        msg = f"Our next meetup is {start_date.humanize()} "
-                        msg += f"from now, on {start_date.day}/{start_date.month}/{start_date.year} "
-                        msg += f"from {start_date.hour}:{start_date.minute} until {end_date.hour}:{end_date.minute}. "
-                        msg += f"We'll be at {venue_name}, and you can find more details about how to get there at {event_url}"
-                        logging.info(msg)
-                        break
-                    else:
-                        msg = "Sorry, no upcoming meetups right now :("
-            else:
-                logging.info(f"Request to Tendenci failed, status code was {response.status}")
-                msg = "Sorry, I can't retrieve details of our next meeting right now :("
-
-            await ctx.send(msg)
+    next_mtg = get_next_meeting()
+    msg = f"Our next meeting is on {dt.datetime.strftime(next_mtg, '%A, %d %B')}"
+    msg = msg + f" between 8pm and 10pm at Woodland Stores, Wyesham"
+    await ctx.send(msg)
 
 @bot.command(name='findit')
 async def findit(ctx, arg):
